e635a88403aef5211715bb900508b5701a1f1ff6
yuantangfei@gmail.com <yuantangfei@gmail.com>
2017-05-03 15:12:52 +0800

    添加了字符串匹配

--- src/PatternMatch.java
+++ src/PatternMatch.java
@@ -1,12 +1,54 @@
 import java.util.Scanner;
 public class PatternMatch {
     public static void main(String[] args) {
+        //System.out.println("***".replaceAll("\\*\\*", "*"));
         Scanner sc = new Scanner(System.in);
         String destStr = sc.nextLine();
         String filterStr = sc.nextLine();
-        System.out.println(isMatch(destStr , filterStr));
+        //System.out.println(isMatch(destStr , filterStr));
+        System.out.println(linearPatternMatch(destStr , filterStr));
     }
 
+    public static boolean linearPatternMatch(String str, String pattern){
+        pattern = pattern.replaceAll("\\*\\*", "*").replaceAll("\\*\\*", "*");
+        //match存与星号后面的第一个相匹配的字符
+        //lastStarIndex存的是上一个星号的位置
+        char[] strSeq = str.toCharArray();
+        char[] patternSeq = pattern.toCharArray();
+        int s = 0, p = 0, match = 0, lastStarIndex = -1;
+        while(s < strSeq.length){
+            //以str的长度为条件，如果p>pattern的长度，那么看最后一次*，继续往前排。
+            //否则，就比较pattern当前的值和str当前的值
+            if(p < patternSeq.length && patternSeq[p] != '*' &&(patternSeq[p] == '?' || patternSeq[p] == strSeq[s])){
+                p++;
+                s++;
+            }
+            else if(p < patternSeq.length && patternSeq[p] == '*'){
+                lastStarIndex = p;
+                p++;
+                match = s;
+            }
+            else if(lastStarIndex != -1){
+                match++;
+                s = match;
+                p = lastStarIndex + 1;
+            }
+        }
+        while(p < patternSeq.length){
+            if(patternSeq[p] != '*'){
+                return false;
+            }
+            p++;
+        }
+        return true;
+    }
+
+    /**
+     * 下面是递归写法，可能有错。时间复杂度太高
+     * @param str
+     * @param pattern
+     * @return
+     */
     public static boolean isMatch(String str, String pattern) {
         if(str.length() == 0 && pattern.length() == 0){
             return true;
---
+++ src/testFinal.java
@@ -0,0 +1,13 @@
+/**
+ * Created by jack on 2017/5/2.
+ */
+public class testFinal {
+    int t;
+    static int i;
+    //final static int j;
+    //final int k;
+    public testFinal(){
+        i = 3;
+
+    }
+}
